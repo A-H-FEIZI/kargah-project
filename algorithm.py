ff52a53e04406f1bb420e9e6dedf79916a62e241
amirhossein <a.h.feizi802@example.com>
2026-05-31 13:08:52 +0330

    add:algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,21 @@
-queue = []
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-queue.append("علی")
-queue.append("رضا")
-queue.append("سارا")
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node10 = Node(10)
+node12 = Node(12)
 
-print(queue.pop(0))
-print(queue.pop(0))
-print(queue)
+node1.next = node2
+node2.next = node3
+node3.next = node10
+node10.next = node12
+
+current = node1
+
+while current != None:
+    print(current.data)
+    current = current.next
