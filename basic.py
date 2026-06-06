488fe41eb0efca17c3a0414bb81fb347c25d62ef
A-H-FEIZI <a.h.feizi802@gmail.com>
2026-06-06 04:34:12 -0700

    basic.py

--- basic.py
+++ basic.py
@@ -1,6 +1,10 @@
-name = "amirhossein"
-age = 22
-city = "tehran"
-print(f"اسم: {name}")
-print(f"سن :{age}")
-print(f"شهر : {city}")
+num1 = 10
+num2 = 3
+num1+num2
+num1-num2
+num1*num2
+num1/num2
+print(f"جمع: {num1 + num2}")
+print(f"تفریق:  {num1-num2}")
+print(f"ضرب:  {num1*num2}")
+print(f"تقسیم: {num1 / num2:.2f}")
