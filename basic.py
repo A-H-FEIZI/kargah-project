acb9da57a088d90f025356d5339a06acfa1633e1
A-H-FEIZI <a.h.feizi802@gmail.com>
2026-06-06 04:54:11 -0700

    basic.py

--- basic.py
+++ basic.py
@@ -1,10 +1,7 @@
-num1 = 10
-num2 = 3
-num1+num2
-num1-num2
-num1*num2
-num1/num2
-print(f"جمع: {num1 + num2}")
-print(f"تفریق:  {num1-num2}")
-print(f"ضرب:  {num1*num2}")
-print(f"تقسیم: {num1 / num2:.2f}")
+score = 75
+if score > 90:
+    print("عالی")
+elif score > 60:
+    print("قبول")
+else:
+    print("مردود")
