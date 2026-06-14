ce0588aa1e214c0afa34e16a69edd96ca41f0394
A-H-FEIZI <a.h.feizi802@gmail.com>
2026-06-14 01:42:50 -0700

    bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,7 +1,7 @@
 def bubble_sort(numbers):
     for i in range(len(numbers)):
         swapped = False
-        for j in range(len(numbers)-1):
+        for j in range(len(numbers) - 1 - i):
             if numbers[j] > numbers[j+1]:
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
                 swapped = True
