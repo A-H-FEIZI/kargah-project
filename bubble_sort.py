b98ed67439845dd7c3c9485bcf393dc84ee52785
A-H-FEIZI <a.h.feizi802@gmail.com>
2026-06-14 01:23:01 -0700

    bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,13 @@
 def bubble_sort(numbers):
     for i in range(len(numbers)):
+        swapped = False
         for j in range(len(numbers)-1):
             if numbers[j] > numbers[j+1]:
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                swapped = True
+        if not swapped:    
+            break               
     return numbers
-user_input = input("اعداد رو وارد کن: ")
+user_input = input("اعداد رو با فاصله وارد کن: ")
 numbers = list(map(int, user_input.split()))
 print(bubble_sort(numbers))
