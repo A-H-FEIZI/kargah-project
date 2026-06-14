2764554025c09ff2b0cae8bfc71cfeb9fcecc2c2
A-H-FEIZI <a.h.feizi802@gmail.com>
2026-06-14 00:28:39 -0700

    bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,7 @@
+def bubble_sort(numbers):
+    for i in range(len(numbers)):
+        for j in range(len(numbers)-1):
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    return numbers
+print(bubble_sort([8, 1, 6, 5, 3, 2]))
